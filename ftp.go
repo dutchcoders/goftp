0d0b7650f1c942f0bae15672b21c787f61a08988
Remco <remco@dutchcoders.io>
2016-07-06 12:35:53 +0200

    fixed issue with lock being copied

--- ftp.go
+++ ftp.go
@@ -207,15 +207,15 @@ func (ftp *FTP) Dele(path string) (err error) {
 }
 
 // secures the ftp connection by using TLS
-func (ftp *FTP) AuthTLS(config tls.Config) error {
+func (ftp *FTP) AuthTLS(config *tls.Config) error {
 	if _, err := ftp.cmd("234", "AUTH TLS"); err != nil {
 		return err
 	}
 
 	// wrap tls on existing connection
-	ftp.tlsconfig = &config
+	ftp.tlsconfig = config
 
-	ftp.conn = tls.Client(ftp.conn, &config)
+	ftp.conn = tls.Client(ftp.conn, config)
 	ftp.writer = bufio.NewWriter(ftp.conn)
 	ftp.reader = bufio.NewReader(ftp.conn)
 
