b989635f227aa8af5ad0e2025511ae5dd84ecf6a
tjf <tjf@megvii.com>
2018-07-03 21:37:56 +0800

    [add]:add connectTLS. (ftp over tls)

--- connect.go
+++ connect.go
@@ -1,10 +1,31 @@
 package goftp
 
 import (
+	"crypto/tls"
 	"bufio"
 	"log"
 	"net"
 )
+
+// ConnectTLS (FTP over TLS).
+func ConnectTLS(addr string, config *tls.Config, debug bool) (*FTP, error) {
+	var err error
+	var conn net.Conn
+
+	if conn, err = net.Dial("tcp", addr); err != nil {
+		return nil, err
+	}
+	conn = tls.Client(conn, config)
+
+	writer := bufio.NewWriter(conn)
+	reader := bufio.NewReader(conn)
+
+	// reader.ReadString('\n')
+	object := &FTP{conn: conn, addr: addr, reader: reader, writer: writer, tlsconfig: config, debug: debug}
+	object.receive()
+
+	return object, nil
+}
 
 // Connect to server at addr (format "host:port"). debug is OFF
 func Connect(addr string) (*FTP, error) {
@@ -18,7 +39,7 @@ func Connect(addr string) (*FTP, error) {
 	writer := bufio.NewWriter(conn)
 	reader := bufio.NewReader(conn)
 
-	//reader.ReadString('\n')
+	// reader.ReadString('\n')
 	object := &FTP{conn: conn, addr: addr, reader: reader, writer: writer, debug: false}
 	object.receive()
 
